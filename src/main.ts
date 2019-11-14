79d2312f79abd17879bdf9f29e839e2a3958d675
yangguang <yangguang@dm-ai.cn>
2019-11-14 18:54:03 +0800

    .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ let transform: d3.ZoomTransform = d3.zoomIdentity;
 
 const canvas: HTMLCanvasElement = document.createElement('canvas') as HTMLCanvasElement;
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
-const devicePixelRatio = window.devicePixelRatio;
+const devicePixelRatio = window.devicePixelRatio || 1;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -19,6 +19,8 @@ canvas.style.height = height + 'px';
 
 canvas.width = viewport.width;
 canvas.height = viewport.height;
+
+ctx.scale(devicePixelRatio, devicePixelRatio);
 
 document.body.appendChild(canvas);
 
@@ -41,8 +43,8 @@ function drawCircle () {
     ctx.clearRect(0, 0, viewport.width, viewport.height);
     ctx.save();
 
-    ctx.translate(transform.x * devicePixelRatio, transform.y * devicePixelRatio);
-    ctx.scale(transform.k * devicePixelRatio, transform.k * devicePixelRatio);
+    ctx.scale(transform.k, transform.k);
+    ctx.translate(transform.x, transform.y);
 
     ctx.beginPath();
     ctx.arc(width / 2, height / 2, 50, 0, Math.PI * 2);
